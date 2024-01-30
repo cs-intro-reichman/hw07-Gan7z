c1943628e084a49e7fb8ac16f57eae87630abb67
Gan7z <oshri.gantz@runi.ac.il>
2024-01-30 13:51:04 +0200

    v.0.0.1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -5,6 +5,7 @@ public class HashTagTokenizer {
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
+		hashTag.toLowerCase();
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
@@ -13,14 +14,20 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
+		for(int i = 0; i < dictionary.length;i++){
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for(int i = 0; i < dictionary.length;i++){
+			if (dictionary[i].equals(word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -33,7 +40,13 @@ public class HashTagTokenizer {
         int N = hashtag.length();
 
         for (int i = 1; i <= N; i++) {
-		
+			System.out.println(hashtag.substring(0, i));
+			if (existInDictionary(hashtag.substring(0, i), dictionary) == true){
+				System.out.println(hashtag.substring(0, i)+"*******");
+				breakHashTag(hashtag.substring(i, N), dictionary);
+				return;
+			}
+			
         }
     }
 
